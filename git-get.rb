260ca5bed5230e2a6a2e04a052e776811722b639
goreleaserbot <goreleaser@carlosbecker.com>
2019-02-13 11:36:25 +0800

    Brew formula update for git-get version v0.3.3

--- git-get.rb
+++ git-get.rb
@@ -2,11 +2,12 @@
 class GitGet < Formula
   desc "Clone git repositories into namespaced directories like go get."
   homepage "https://github.com/yi-jiayu/git-get"
-  url "https://github.com/yi-jiayu/git-get/releases/download/v0.2.2/git-get_0.2.2_darwin_amd64.tar.gz"
-  version "0.2.2"
-  sha256 "19d5698b836e0126a157e08fc877c4ffcc16a12a940271cf783ecd1f431b5261"
+  url "https://github.com/yi-jiayu/git-get/releases/download/v0.3.3/git-get_0.3.3_darwin_amd64.tar.gz"
+  version "0.3.3"
+  sha256 "c3fb8e2a8ae3230ec3b82e55fdd413af5f18ba9789752fbe85c20afab5a8f8a0"
 
   def install
     bin.install "git-get"
+    man1.install "git-get.1"
   end
 end
