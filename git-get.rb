0570c81986c60e529ba1d4209d1b6443fc031eae
goreleaserbot <goreleaser@carlosbecker.com>
2021-11-19 13:04:56 +0800

    Brew formula update for git-get version v0.6.2

--- git-get.rb
+++ git-get.rb
@@ -5,33 +5,47 @@
 class GitGet < Formula
   desc "Clone git repositories into namespaced directories like go get."
   homepage "https://github.com/yi-jiayu/git-get"
-  version "0.6.1"
-  bottle :unneeded
+  version "0.6.2"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/yi-jiayu/git-get/releases/download/v0.6.1/git-get_0.6.1_darwin_amd64.tar.gz"
-      sha256 "3ab1512790a986037a2aeca35969907adcc45429e185e9693d280de4a7145ba9"
+      url "https://github.com/yi-jiayu/git-get/releases/download/v0.6.2/git-get_0.6.2_darwin_amd64.tar.gz"
+      sha256 "e73ee337d7b8e30afc3b106c193f0a8f57ecb8e43ea4c40af1c93a81fc066ec5"
+
+      def install
+        bin.install "git-get"
+        man1.install "git-get.1"
+      end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/yi-jiayu/git-get/releases/download/v0.6.1/git-get_0.6.1_darwin_arm64.tar.gz"
-      sha256 "3f908ee0e5ae725dad145294fd12349fdbb0f84aa695c7a1e436cc7c7a51329e"
+      url "https://github.com/yi-jiayu/git-get/releases/download/v0.6.2/git-get_0.6.2_darwin_arm64.tar.gz"
+      sha256 "df81ae0c9f4ff76ae80b9fc35623b7744907d7eeb272cd5eb146381cc7142056"
+
+      def install
+        bin.install "git-get"
+        man1.install "git-get.1"
+      end
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/yi-jiayu/git-get/releases/download/v0.6.1/git-get_0.6.1_linux_amd64.tar.gz"
-      sha256 "a093b1b82ec0b3216041b72d8a1a838888a2889a397e3a2794bee0ea4965d7d9"
-    end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/yi-jiayu/git-get/releases/download/v0.6.1/git-get_0.6.1_linux_arm64.tar.gz"
-      sha256 "8e6f7837ab1523d534853a966209baa881f87fe7834eb93c51116cce2cd93611"
-    end
-  end
+      url "https://github.com/yi-jiayu/git-get/releases/download/v0.6.2/git-get_0.6.2_linux_arm64.tar.gz"
+      sha256 "e82daa0d33e113e4623c0f3864818902af722251a671f96119089375dec87fa9"
 
-  def install
-    bin.install "git-get"
-    man1.install "git-get.1"
+      def install
+        bin.install "git-get"
+        man1.install "git-get.1"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/yi-jiayu/git-get/releases/download/v0.6.2/git-get_0.6.2_linux_amd64.tar.gz"
+      sha256 "68875dea68295f78920b2e0c5e28325101bac6bffa2a3c21f8f8fa13850fc5ec"
+
+      def install
+        bin.install "git-get"
+        man1.install "git-get.1"
+      end
+    end
   end
 end
